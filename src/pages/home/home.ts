f4088794936cedbdb98e0a9ce828536cc657e7b2
cuongdq.payment <cuongdq.payment@gmail.com>
2018-11-08 10:20:14 +0700

    HEROKU Deployment by cuongdq

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, LoadingController, Searchbar, ToastController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Subscription } from 'rxjs/Subscription';
 import { Location } from '../../models/location';
