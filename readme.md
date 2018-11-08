180070e20d667805fed1410872eb8ad33443880e
cuongdq.payment <cuongdq.payment@gmail.com>
2018-11-08 10:06:05 +0700

    heroku ionic server cuongdq

---
+++ readme.md
@@ -0,0 +1,20 @@
+install ionic on heroku as website:
+copy: 
+"devDependencies": {               
+    #-- all include for device - but www in heroku so: 
+    "@ionic/app-scripts": "3.2.0", -- copy
+    "typescript": "~2.6.2"         -- copy
+  },
+
+into:
+"dependencies": {
+    ....,
+    "@ionic/app-scripts": "3.2.0", 
+    "typescript": "~2.6.2"        
+}
+
+make: server.js
+
+make: Procfile
+
+make: herokuDeployment.sh
---
+++ server.js
@@ -0,0 +1,28 @@
+//su dung server cho heroku bao thu muc chay web la www
+var express = require("express");
+var app = express();
+
+//su dung cac thanh phan
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(cors());
+////////////////////////////
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static("www"));
+var port = process.env.PORT || 5000;
+app.set('port',port);
+app.listen(app.get('port'), function () {
+    console.log('listening in http://cuongdqionic.herokuapp.com:' + port);
+});
